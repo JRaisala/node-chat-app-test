e630b99655aa06438dc58bcbfbb152535621ad10
Juha Räisälä <juha.raisala@gmail.com>
2019-01-28 09:38:07 +0200

    Reformat with Async/await

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
+mongoose.Promise = Promise
+
 var dbUrl = 'mongodb://user:user123@ds051831.mlab.com:51831/learning-node'
 
 var Message = mongoose.model('Message', {
@@ -22,17 +24,28 @@ app.get('/messages', (req,res) => {
 	})
 })
 
-app.post('/messages', (req,res) => {
+app.post('/messages', async (req,res) => {
 	var message = new Message(req.body)
 
-	message.save((err) => {
-		if(err)
-		sendStatus(500)
+	var savedMessage = await message.save()
 
-		io.emit('message', req.body)
+	console.log('saved')
+
+	var censored = await Message.findOne({ message: 'badword' })
+
+		if (censored) 
+			await Message.deleteOne({ _id: censored.id })
+		else
+			io.emit('message', req.body)
+
 		res.sendStatus(200)
-	})
+
+	// .catch((err) => {
+	// 	res.sendStatus(500)
+	// 	return console.error(err)
+	// })
 })
+
 
 io.on('connection', (socket) => {
 	console.log('a user connected')
